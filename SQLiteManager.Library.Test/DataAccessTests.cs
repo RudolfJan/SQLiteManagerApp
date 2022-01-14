ed810ddcea72c37890d14a5237223b9216ee8a53
pjlplourde <pierre@spartancsharp.net>
2022-01-14 10:00:37 -0500

    Added SQLite DataAccess class to library along with NuGet packages for Dapper and SQLite; added Unit Test Class and scaffold of test methods.

---
+++ SQLiteManager.Library.Test/DataAccessTests.cs
@@ -0,0 +1,33 @@
+using System;
+
+using Xunit;
+
+namespace SQLiteManager.Library.Test
+{
+	public class DataAccessTests
+	{
+		[Fact]
+		public void LoadDataReturnsData()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public void LoadDataAsyncReturnsAsyncData()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public void SaveDataSavesData()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public void SaveDataAsyncSavesDataAsynchronously()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
